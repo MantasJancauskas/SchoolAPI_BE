d9d2b58f8a0eec514187d54052fa689c9ec07aed
MantasJancauskas <mantas.jancauskas1@gmail.com>
2022-11-24 09:53:25 +0200

    fixed migration models

---
+++ database/migrations/2022_11_24_073458_create_aplications_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('aplications', function (Blueprint $table) {
+            $table->id('school_id')->references('id')->on('schools');
+            $table->foreignId('children_id')->nullable()->references('id')->on('children_aplications')
+                ->onDelete('set null')
+                ->onUpdate('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('aplications');
+    }
+};
