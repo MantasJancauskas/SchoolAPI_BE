1590710b6e72cfaf086e600f043340946254a833
MantasJancauskas <mantas.jancauskas1@gmail.com>
2022-11-24 09:28:42 +0200

    Made migration models

---
+++ database/migrations/2022_11_24_064424_create_children_aplications_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('children_aplications', function (Blueprint $table) {
+            $table->id('id');
+            $table->string('birth_day');
+            $table->string('name');
+            $table->integer('class');
+            $table->foreignId('children_id')->nullable()->references('id')->on('schools')
+                    ->onDelete('set Null')
+                    ->onUpdate('restrict');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('children_aplications');
+    }
+};
